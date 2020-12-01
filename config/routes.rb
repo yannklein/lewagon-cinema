fcc4d678ff140ec403644e7bfa35c2a372bef70a
Yann Klein <yann.klein@me.com>
2020-12-01 15:52:18 +0900

    create movies controller and actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'movies/index'
+  get 'movies/new'
+  get 'movies/create'
+  get 'movies/edit'
+  get 'movies/update'
+  get 'movies/destroy'
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
