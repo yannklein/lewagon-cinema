94bff7c3fbf385f6f6c3e92d6f4f76609f6020d1
Yann Klein <yann.klein@me.com>
2021-06-06 14:32:05 +0900

    fix scroll issue in movie list

--- app/javascript/packs/openMovieList.js
+++ app/javascript/packs/openMovieList.js
@@ -1,9 +1,11 @@
 const openMovieWheelEvent = (event) => {
   const movieList = document.querySelector(".cinema-movies");
-  console.log(event);
+  const MovieListContent =  document.querySelector(".cinema-movies-content");
+  // console.log(MovieListContent.scrollTop);
   if (event.deltaY > 0) {
     movieList.classList.add("open");
-  } else {
+  } 
+  else if (MovieListContent.scrollTop == 0){
     movieList.classList.remove("open");
   }
 }
@@ -15,19 +17,6 @@ const openMovieList = () => {
     movieList.classList.toggle("open");
   });
   document.addEventListener("wheel", openMovieWheelEvent, true);
-
-  const iframe = document.querySelector('iframe');
-  const iframeCover = document.querySelector('.iframe-cover');
-  iframe.addEventListener('load', function(){
-    iframeCover.addEventListener("mouseover", () => { 
-      iframeCover.classList.add('to-the-back');
-      // iframe.contentDocument.click();
-      console.log('overed', iframe);
-    });
-    iframe.addEventListener("wheel", () => { 
-      console.log('wheeled', iframe);
-    }, true);
-  });
 }
 
 export { openMovieList };
