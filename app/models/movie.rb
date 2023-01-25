e041ba9605f09ac2f36da7ab3f2f3f93d5a7fd7a
Yann Klein <yann.klein@me.com>
2023-01-25 15:42:46 +0900

    update models to match new shared db

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,6 @@
 class Movie < ApplicationRecord
-  belongs_to :project_category
+  self.table_name = "lw_cinema_movies"
+  belongs_to :project_category, foreign_key: "lw_cinema_project_category_id"
   validates :title, presence: true
   validates :description, presence: true
   validates :batch, presence: true
--- app/models/project_category.rb
+++ app/models/project_category.rb
@@ -1,3 +1,4 @@
 class ProjectCategory < ApplicationRecord
-  has_many :movies
+  self.table_name = "lw_cinema_project_categories"
+  has_many :movies, foreign_key: "lw_cinema_project_category_id"
 end
